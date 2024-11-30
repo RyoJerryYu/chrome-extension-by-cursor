5333e03ae6b28e29f90a037016c8c151a410fdba
Ryo <36154873+RyoJerryYu@users.noreply.github.com>
2024-11-30 17:08:31 +0800

    add token for client

--- src/grpc/client.ts
+++ src/grpc/client.ts
@@ -1,13 +1,56 @@
-import { createChannel, createClientFactory, FetchTransport } from "nice-grpc-web";
+import {
+  CallOptions,
+  ClientMiddleware,
+  ClientMiddlewareCall,
+  createChannel,
+  createClientFactory,
+  FetchTransport,
+  Metadata,
+} from "nice-grpc-web";
 import { MemoServiceDefinition } from "../../proto/src/proto/api/v1/memo_service";
 
 const channel = createChannel(
-  'http://localhost:5230',  // Update this to your backend URL
+  "http://localhost:5230", // Update this to your backend URL
   FetchTransport({
     credentials: "include",
   })
 );
 
-const clientFactory = createClientFactory();
+const loggingMiddleware: ClientMiddleware =
+  async function* devtoolsLoggingMiddleware<Request, Response>(
+    call: ClientMiddlewareCall<Request, Response>,
+    options: CallOptions
+  ): AsyncGenerator<Response, Response | void, undefined> {
+    const req = call.request;
+    let resp;
+    try {
+      resp = yield* call.next(call.request, options);
+      return resp;
+    } finally {
+      console.log(
+        `gRPC to ${call.method.path}\n\nrequest:\n${JSON.stringify(
+          req
+        )}\n\nresponse:\n${JSON.stringify(resp)}`
+      );
+    }
+  };
 
-export const memoServiceClient = clientFactory.create(MemoServiceDefinition, channel); 
+const bearerAuthMiddleware: (token: string) => ClientMiddleware = (token) => {
+  return (call, options) =>
+    call.next(call.request, {
+      ...options,
+      metadata: Metadata(options.metadata).set(
+        "authorization",
+        `Bearer ${token}`
+      ),
+    });
+};
+
+const clientFactory = createClientFactory()
+  .use(loggingMiddleware)
+  .use(bearerAuthMiddleware("eyJhbGciOiJIUzI1NiIsImtpZCI6InYxIiwidHlwIjoiSldUIn0.eyJuYW1lIjoiUnlvSmVycnlZdSIsImlzcyI6Im1lbW9zIiwic3ViIjoiMSIsImF1ZCI6WyJ1c2VyLmFjY2Vzcy10b2tlbiJdLCJpYXQiOjE3MTY1Mzk2MTF9.03Wn892HSe8vtS4hiQeGfZ1cwyko5l8xJ3kH_AlYeuE"));
+
+export const memoServiceClient = clientFactory.create(
+  MemoServiceDefinition,
+  channel
+);
